ce8c646fb6eb65747f5bae2a946df7f9d6876604
石丸大貴 <>
2020-12-04 11:49:02 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/works">Works</router-link> |
+      <router-link to="contact">Contact</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body{
+  background-color: #111;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #fff;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <header class="header">
+      <div class="header__ttl">
+        <img src="@/assets/ttl.png" alt="タイトル">
+      </div>  <!-- header__ttl -->
+      <h2 class="header__subttl">田舎の農業高校からWebエンジニアへ</h2>
+    </header> <!-- header__subttl -->
+    <main>
+      <div class="about">
+        <h2 class="nav_ttl"></h2>
+        <div class="about__wrapper">
+          <div class="about__wrapper__wrap">
+            <div class="about__wrapper__wrap__leftbox">
+              <img src="@/assets/about_my_img.png" alt="">
+              <p>Platform ・ SNS</p>
+              <a href="#" class="about__wrapper__wrap__leftbox__github">
+                <img src="@/assets/Github_icon-icons.com_67091.png" alt="">
+                <p>to Github</p>
+              </a> <!-- about__wrapper__wrap__leftbox__github -->
+              <a href="#" class="about__wrapper__wrap__leftbox__wantedly">
+                <img src="@/assets/mark_visit.png" alt="">
+                <p>to Wantedly</p>
+              </a>  <!-- about__wrapper__wrap__leftbox__wantedly -->
+            </div>  <!-- about__wrapper__wrap__leftbox -->
+          </div>  <!-- about__wrapper__wrap -->
+        </div>  <!-- about__wrapper -->
+      </div>  <!-- about -->
+    </main> <!-- main -->
+  </div>  <!-- wrapper -->
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+$fontColor: #fff;
+
+h2, p, a{
+  color: $fontColor;
+}
+</style>
