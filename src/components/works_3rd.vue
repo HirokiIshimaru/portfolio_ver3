aa5d4dc285e3ac64baf1622d199069d94773f41f
石丸大貴 <[hirokiishimaru6132@gmail.com]>
2021-02-24 17:25:59 +0900

    textstyle変更

--- src/components/works_3rd.vue
+++ src/components/works_3rd.vue
@@ -6,9 +6,9 @@
 
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@300;400;700&family=Noto+Serif+TC:wght@200;400;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@300&display=swap');
 @import '@/sass/works-list.scss';
-$jpFont: 'Noto Sans JP', sans-serif;
+$jpFont: 'Noto Serif JP', serif;
 $enFont: 'Noto Serif TC','Noto Serif JP', serif;
 $fontColor: #fff;
 
